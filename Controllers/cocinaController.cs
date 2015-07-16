fe3b2f5c9a8e346bfb3f8bc1c71f7abeae7db1e8
alexisshriov <alexis.st21@gmail.com>
2015-07-16 10:27:47 -0500

    project copied

---
+++ Controllers/cocinaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Filters;
+
+namespace WebApplication3.Controllers
+{
+    public class cocinaController : Controller
+    {
+        [MyFilter]
+        public ActionResult Search(string plato)
+        {
+            return Content("This is a search");
+        }
+
+    
+    }
+}
